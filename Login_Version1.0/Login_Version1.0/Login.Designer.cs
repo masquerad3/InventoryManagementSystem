947b0731105b0f4907f2abd66669ed39c92ba53b
Steven John Masangcay <107854493+JohnEsteban09@users.noreply.github.com>
2025-06-09 22:56:29 +0800

    Added the Login V.1 folder

---
+++ Login_Version1.0/Login_Version1.0/Login.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Login_Version1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cuiPanel1 = new CuoreUI.Controls.cuiPanel();
+            cuiLabel6 = new CuoreUI.Controls.cuiLabel();
+            cuiLabel5 = new CuoreUI.Controls.cuiLabel();
+            cuiLabel4 = new CuoreUI.Controls.cuiLabel();
+            cuiLabel3 = new CuoreUI.Controls.cuiLabel();
+            cuiButton1 = new CuoreUI.Controls.cuiButton();
+            cuiTextBox2 = new CuoreUI.Controls.cuiTextBox();
+            cuiTextBox1 = new CuoreUI.Controls.cuiTextBox();
+            cuiLabel1 = new CuoreUI.Controls.cuiLabel();
+            cuiLabel2 = new CuoreUI.Controls.cuiLabel();
+            cuiPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // cuiPanel1
+            // 
+            cuiPanel1.Controls.Add(cuiLabel6);
+            cuiPanel1.Controls.Add(cuiLabel5);
+            cuiPanel1.Controls.Add(cuiLabel4);
+            cuiPanel1.Controls.Add(cuiLabel3);
+            cuiPanel1.Controls.Add(cuiButton1);
+            cuiPanel1.Controls.Add(cuiTextBox2);
+            cuiPanel1.Controls.Add(cuiTextBox1);
+            cuiPanel1.Location = new Point(691, 41);
+            cuiPanel1.Name = "cuiPanel1";
+            cuiPanel1.OutlineThickness = 1F;
+            cuiPanel1.PanelColor = Color.WhiteSmoke;
+            cuiPanel1.PanelOutlineColor = Color.FromArgb(22, 22, 22);
+            cuiPanel1.Rounding = new Padding(8);
+            cuiPanel1.Size = new Size(550, 630);
+            cuiPanel1.TabIndex = 0;
+            // 
+            // cuiLabel6
+            // 
+            cuiLabel6.Content = "Sign\\ up\\ here";
+            cuiLabel6.BackColor = Color.Transparent;
+            cuiLabel6.Font = new Font("Arial", 9.75F, FontStyle.Bold | FontStyle.Underline);
+            cuiLabel6.HorizontalAlignment = StringAlignment.Center;
+            cuiLabel6.Location = new Point(180, 430);
+            cuiLabel6.Name = "cuiLabel6";
+            cuiLabel6.Size = new Size(200, 20);
+            cuiLabel6.TabIndex = 4;
+            cuiLabel6.VerticalAlignment = StringAlignment.Near;
+            // 
+            // cuiLabel5
+            // 
+            cuiLabel5.Content = "Don't\\ have\\ an\\ account\\ yet\\?";
+            cuiLabel5.BackColor = Color.Transparent;
+            cuiLabel5.HorizontalAlignment = StringAlignment.Center;
+            cuiLabel5.Location = new Point(180, 400);
+            cuiLabel5.Name = "cuiLabel5";
+            cuiLabel5.Size = new Size(200, 30);
+            cuiLabel5.TabIndex = 3;
+            cuiLabel5.VerticalAlignment = StringAlignment.Near;
+            // 
+            // cuiLabel4
+            // 
+            cuiLabel4.BackColor = Color.Transparent;
+            cuiLabel4.Content = "Welcome\\ to\\ Inventory\\ Management\\ System";
+            cuiLabel4.Font = new Font("Arial", 10F);
+            cuiLabel4.HorizontalAlignment = StringAlignment.Center;
+            cuiLabel4.Location = new Point(129, 180);
+            cuiLabel4.Margin = new Padding(4, 3, 4, 3);
+            cuiLabel4.Name = "cuiLabel4";
+            cuiLabel4.Size = new Size(300, 30);
+            cuiLabel4.TabIndex = 0;
+            cuiLabel4.VerticalAlignment = StringAlignment.Near;
+            // 
+            // cuiLabel3
+            // 
+            cuiLabel3.BackColor = Color.Transparent;
+            cuiLabel3.Content = "Hi\\ User!";
+            cuiLabel3.Font = new Font("Arial", 20F, FontStyle.Bold);
+            cuiLabel3.HorizontalAlignment = StringAlignment.Center;
+            cuiLabel3.Location = new Point(129, 150);
+            cuiLabel3.Margin = new Padding(4, 3, 4, 3);
+            cuiLabel3.Name = "cuiLabel3";
+            cuiLabel3.Size = new Size(300, 50);
+            cuiLabel3.TabIndex = 0;
+            cuiLabel3.VerticalAlignment = StringAlignment.Near;
+            // 
+            // cuiButton1
+            // 
+            cuiButton1.CheckButton = false;
+            cuiButton1.Checked = false;
+            cuiButton1.CheckedBackground = Color.FromArgb(255, 106, 0);
+            cuiButton1.CheckedForeColor = Color.White;
+            cuiButton1.CheckedImageTint = Color.White;
+            cuiButton1.CheckedOutline = Color.FromArgb(255, 106, 0);
+            cuiButton1.Content = "Login";
+            cuiButton1.BackColor = Color.Transparent;
+            cuiButton1.DialogResult = DialogResult.None;
+            cuiButton1.Font = new Font("Arial", 9.75F, FontStyle.Bold);
+            cuiButton1.ForeColor = Color.White;
+            cuiButton1.HoverBackground = Color.White;
+            cuiButton1.HoveredImageTint = Color.White;
+            cuiButton1.HoverForeColor = Color.Black;
+            cuiButton1.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            cuiButton1.Image = null;
+            cuiButton1.ImageAutoCenter = true;
+            cuiButton1.ImageExpand = new Point(0, 0);
+            cuiButton1.ImageOffset = new Point(0, 0);
+            cuiButton1.Location = new Point(48, 514);
+            cuiButton1.Name = "cuiButton1";
+            cuiButton1.NormalBackground = Color.Black;
+            cuiButton1.NormalForeColor = Color.White;
+            cuiButton1.NormalImageTint = Color.White;
+            cuiButton1.NormalOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton1.OutlineThickness = 1F;
+            cuiButton1.PressedBackground = Color.WhiteSmoke;
+            cuiButton1.PressedForeColor = Color.FromArgb(32, 32, 32);
+            cuiButton1.PressedImageTint = Color.White;
+            cuiButton1.PressedOutline = Color.FromArgb(64, 128, 128, 128);
+            cuiButton1.Rounding = new Padding(8);
+            cuiButton1.Size = new Size(466, 45);
+            cuiButton1.TabIndex = 2;
+            cuiButton1.TextAlignment = StringAlignment.Center;
+            cuiButton1.TextOffset = new Point(0, 0);
+            // 
+            // cuiTextBox2
+            // 
+            cuiTextBox2.BackColor = Color.Transparent;
+            cuiTextBox2.BackgroundColor = Color.White;
+            cuiTextBox2.BorderColor = Color.FromArgb(128, 128, 128, 128);
+            cuiTextBox2.Content = "";
+            cuiTextBox2.FocusBackgroundColor = Color.White;
+            cuiTextBox2.FocusBorderColor = Color.FromArgb(255, 106, 0);
+            cuiTextBox2.FocusImageTint = Color.White;
+            cuiTextBox2.Font = new Font("Microsoft Sans Serif", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cuiTextBox2.ForeColor = Color.Gray;
+            cuiTextBox2.Image = null;
+            cuiTextBox2.ImageExpand = new Point(0, 0);
+            cuiTextBox2.ImageOffset = new Point(0, 0);
+            cuiTextBox2.Location = new Point(48, 306);
+            cuiTextBox2.Margin = new Padding(4);
+            cuiTextBox2.Multiline = false;
+            cuiTextBox2.Name = "cuiTextBox2";
+            cuiTextBox2.NormalImageTint = Color.White;
+            cuiTextBox2.Padding = new Padding(15, 15, 15, 0);
+            cuiTextBox2.PasswordChar = true;
+            cuiTextBox2.PlaceholderColor = Color.Gray;
+            cuiTextBox2.PlaceholderText = "Password";
+            cuiTextBox2.Rounding = new Padding(8);
+            cuiTextBox2.Size = new Size(466, 45);
+            cuiTextBox2.TabIndex = 1;
+            cuiTextBox2.TextOffset = new Size(0, 0);
+            cuiTextBox2.UnderlinedStyle = true;
+            // 
+            // cuiTextBox1
+            // 
+            cuiTextBox1.BackColor = Color.Transparent;
+            cuiTextBox1.BackgroundColor = Color.White;
+            cuiTextBox1.BorderColor = Color.DarkGray;
+            cuiTextBox1.Content = "";
+            cuiTextBox1.FocusBackgroundColor = Color.White;
+            cuiTextBox1.FocusBorderColor = Color.FromArgb(255, 106, 0);
+            cuiTextBox1.FocusImageTint = Color.White;
+            cuiTextBox1.Font = new Font("Microsoft Sans Serif", 9.5F);
+            cuiTextBox1.ForeColor = Color.Gray;
+            cuiTextBox1.Image = null;
+            cuiTextBox1.ImageExpand = new Point(0, 0);
+            cuiTextBox1.ImageOffset = new Point(0, 0);
+            cuiTextBox1.Location = new Point(48, 239);
+            cuiTextBox1.Margin = new Padding(4);
+            cuiTextBox1.Multiline = false;
+            cuiTextBox1.Name = "cuiTextBox1";
+            cuiTextBox1.NormalImageTint = Color.White;
+            cuiTextBox1.Padding = new Padding(15, 15, 15, 0);
+            cuiTextBox1.PasswordChar = false;
+            cuiTextBox1.PlaceholderColor = Color.Gray;
+            cuiTextBox1.PlaceholderText = "Email";
+            cuiTextBox1.Rounding = new Padding(8);
+            cuiTextBox1.Size = new Size(466, 45);
+            cuiTextBox1.TabIndex = 0;
+            cuiTextBox1.TextOffset = new Size(0, 0);
+            cuiTextBox1.UnderlinedStyle = true;
+            // 
+            // cuiLabel1
+            // 
+            cuiLabel1.Content = "Inventory\\ Management\\ System";
+            cuiLabel1.Font = new Font("Arial", 36F, FontStyle.Bold);
+            cuiLabel1.HorizontalAlignment = StringAlignment.Near;
+            cuiLabel1.Location = new Point(88, 41);
+            cuiLabel1.Margin = new Padding(4, 3, 3, 3);
+            cuiLabel1.Name = "cuiLabel1";
+            cuiLabel1.Size = new Size(400, 200);
+            cuiLabel1.TabIndex = 1;
+            cuiLabel1.VerticalAlignment = StringAlignment.Near;
+            // 
+            // cuiLabel2
+            // 
+            cuiLabel2.Content = "Manage\\ your\\ products\\ easily\\ and\\ efficiently!";
+            cuiLabel2.Font = new Font("Arial", 10F, FontStyle.Bold);
+            cuiLabel2.HorizontalAlignment = StringAlignment.Near;
+            cuiLabel2.Location = new Point(88, 230);
+            cuiLabel2.Margin = new Padding(4, 3, 4, 3);
+            cuiLabel2.Name = "cuiLabel2";
+            cuiLabel2.Size = new Size(300, 54);
+            cuiLabel2.TabIndex = 2;
+            cuiLabel2.VerticalAlignment = StringAlignment.Near;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightBlue;
+            ClientSize = new Size(1280, 720);
+            Controls.Add(cuiLabel2);
+            Controls.Add(cuiLabel1);
+            Controls.Add(cuiPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            cuiPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialization code here
+        }
+
+        #endregion
+
+        private CuoreUI.Controls.cuiPanel cuiPanel1;
+        private CuoreUI.Controls.cuiLabel cuiLabel1;
+        private CuoreUI.Controls.cuiTextBox cuiTextBox1;
+        private CuoreUI.Controls.cuiLabel cuiLabel2;
+        private CuoreUI.Controls.cuiTextBox cuiTextBox2;
+        private CuoreUI.Controls.cuiButton cuiButton1;
+        private CuoreUI.Controls.cuiLabel cuiLabel4;
+        private CuoreUI.Controls.cuiLabel cuiLabel3;
+        private CuoreUI.Controls.cuiLabel cuiLabel5;
+        private CuoreUI.Controls.cuiLabel cuiLabel6;
+    }
+}
---
+++ Login_Version1.0/Login_Version1.0/Login.cs
@@ -0,0 +1,10 @@
+namespace Login_Version1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}

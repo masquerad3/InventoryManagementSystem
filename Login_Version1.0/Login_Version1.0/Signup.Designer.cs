01d1c428d6042d142f14367dcb4a755ec6742cfe
Steven John Masangcay <107854493+JohnEsteban09@users.noreply.github.com>
2025-06-10 13:45:16 +0800

    Added Signup cs, but no logic yet

---
+++ Login_Version1.0/Login_Version1.0/Signup.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace Login_Version1._0
+{
+    partial class Signup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            CuoreUI.Controls.cuiLabel cuiLabel1;
+            cuiPanel1 = new CuoreUI.Controls.cuiPanel();
+            TaCCheckBox = new CuoreUI.Controls.cuiCheckbox();
+            PasswordTextBox2 = new CuoreUI.Controls.cuiTextBox();
+            PasswordTextBox1 = new CuoreUI.Controls.cuiTextBox();
+            EmailTextBox1 = new CuoreUI.Controls.cuiTextBox();
+            NameTextBox2 = new CuoreUI.Controls.cuiTextBox();
+            NameTextBox1 = new CuoreUI.Controls.cuiTextBox();
+            RegisterButton = new CuoreUI.Controls.cuiButton();
+            SignupPanelText = new CuoreUI.Controls.cuiLabel();
+            SingupHeader = new CuoreUI.Controls.cuiLabel();
+            cuiLabel2 = new CuoreUI.Controls.cuiLabel();
+            cuiLabel1 = new CuoreUI.Controls.cuiLabel();
+            cuiPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // cuiLabel1
+            // 
+            cuiLabel1.BackColor = Color.Transparent;
+            cuiLabel1.Content = "Welcome\\ to\\ Inventory\\ Management\\ System";
+            cuiLabel1.Font = new Font("Arial", 10F);
+            cuiLabel1.HorizontalAlignment = StringAlignment.Center;
+            cuiLabel1.Location = new Point(130, 109);
+            cuiLabel1.Margin = new Padding(4, 3, 4, 3);
+            cuiLabel1.Name = "cuiLabel1";
+            cuiLabel1.Size = new Size(300, 30);
+            cuiLabel1.TabIndex = 4;
+            cuiLabel1.VerticalAlignment = StringAlignment.Near;
+            // 
+            // cuiPanel1
+            // 
+            cuiPanel1.BackColor = Color.Transparent;
+            cuiPanel1.Controls.Add(TaCCheckBox);
+            cuiPanel1.Controls.Add(PasswordTextBox2);
+            cuiPanel1.Controls.Add(PasswordTextBox1);
+            cuiPanel1.Controls.Add(EmailTextBox1);
+            cuiPanel1.Controls.Add(NameTextBox2);
+            cuiPanel1.Controls.Add(NameTextBox1);
+            cuiPanel1.Controls.Add(RegisterButton);
+            cuiPanel1.Controls.Add(cuiLabel1);
+            cuiPanel1.Controls.Add(SignupPanelText);
+            cuiPanel1.Location = new Point(691, 41);
+            cuiPanel1.Name = "cuiPanel1";
+            cuiPanel1.OutlineThickness = 1F;
+            cuiPanel1.PanelColor = Color.WhiteSmoke;
+            cuiPanel1.PanelOutlineColor = Color.FromArgb(22, 22, 22);
+            cuiPanel1.Rounding = new Padding(8);
+            cuiPanel1.Size = new Size(550, 630);
+            cuiPanel1.TabIndex = 0;
+            // 
+            // TaCCheckBox
+            // 
+            TaCCheckBox.BackColor = Color.WhiteSmoke;
+            TaCCheckBox.Checked = false;
+            TaCCheckBox.CheckedForeground = Color.Black;
+            TaCCheckBox.CheckedOutlineColor = Color.Black;
+            TaCCheckBox.CheckedSymbolColor = Color.White;
+            TaCCheckBox.Content = "I accept Terms & Conditions";
+            TaCCheckBox.Location = new Point(46, 476);
+            TaCCheckBox.MinimumSize = new Size(16, 16);
+            TaCCheckBox.Name = "TaCCheckBox";
+            TaCCheckBox.OutlineStyle = true;
+            TaCCheckBox.OutlineThickness = 1F;
+            TaCCheckBox.Rounding = 5;
+            TaCCheckBox.ShowSymbols = true;
+            TaCCheckBox.Size = new Size(300, 20);
+            TaCCheckBox.TabIndex = 14;
+            TaCCheckBox.Text = "cuiCheckbox1";
+            TaCCheckBox.UncheckedForeground = Color.Empty;
+            TaCCheckBox.UncheckedOutlineColor = Color.Gray;
+            TaCCheckBox.UncheckedSymbolColor = Color.Empty;
+            // 
+            // PasswordTextBox2
+            // 
+            PasswordTextBox2.BackColor = Color.Transparent;
+            PasswordTextBox2.BackgroundColor = Color.White;
+            PasswordTextBox2.BorderColor = Color.FromArgb(128, 128, 128, 128);
+            PasswordTextBox2.Content = "";
+            PasswordTextBox2.FocusBackgroundColor = Color.White;
+            PasswordTextBox2.FocusBorderColor = Color.Black;
+            PasswordTextBox2.FocusImageTint = Color.White;
+            PasswordTextBox2.Font = new Font("Arial", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            PasswordTextBox2.ForeColor = Color.Gray;
+            PasswordTextBox2.Image = null;
+            PasswordTextBox2.ImageExpand = new Point(0, 0);
+            PasswordTextBox2.ImageOffset = new Point(0, 0);
+            PasswordTextBox2.Location = new Point(46, 413);
+            PasswordTextBox2.Margin = new Padding(4);
+            PasswordTextBox2.Multiline = false;
+            PasswordTextBox2.Name = "PasswordTextBox2";
+            PasswordTextBox2.NormalImageTint = Color.White;
+            PasswordTextBox2.Padding = new Padding(15, 15, 15, 0);
+            PasswordTextBox2.PasswordChar = true;
+            PasswordTextBox2.PlaceholderColor = Color.Gray;
+            PasswordTextBox2.PlaceholderText = "Confirm Password";
+            PasswordTextBox2.Rounding = new Padding(8);
+            PasswordTextBox2.Size = new Size(466, 45);
+            PasswordTextBox2.TabIndex = 13;
+            PasswordTextBox2.TextOffset = new Size(0, 0);
+            PasswordTextBox2.UnderlinedStyle = true;
+            // 
+            // PasswordTextBox1
+            // 
+            PasswordTextBox1.BackColor = Color.Transparent;
+            PasswordTextBox1.BackgroundColor = Color.White;
+            PasswordTextBox1.BorderColor = Color.FromArgb(128, 128, 128, 128);
+            PasswordTextBox1.Content = "";
+            PasswordTextBox1.FocusBackgroundColor = Color.White;
+            PasswordTextBox1.FocusBorderColor = Color.Black;
+            PasswordTextBox1.FocusImageTint = Color.White;
+            PasswordTextBox1.Font = new Font("Arial", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            PasswordTextBox1.ForeColor = Color.Gray;
+            PasswordTextBox1.Image = null;
+            PasswordTextBox1.ImageExpand = new Point(0, 0);
+            PasswordTextBox1.ImageOffset = new Point(0, 0);
+            PasswordTextBox1.Location = new Point(46, 348);
+            PasswordTextBox1.Margin = new Padding(4);
+            PasswordTextBox1.Multiline = false;
+            PasswordTextBox1.Name = "PasswordTextBox1";
+            PasswordTextBox1.NormalImageTint = Color.White;
+            PasswordTextBox1.Padding = new Padding(15, 15, 15, 0);
+            PasswordTextBox1.PasswordChar = true;
+            PasswordTextBox1.PlaceholderColor = Color.Gray;
+            PasswordTextBox1.PlaceholderText = "Password";
+            PasswordTextBox1.Rounding = new Padding(8);
+            PasswordTextBox1.Size = new Size(466, 45);
+            PasswordTextBox1.TabIndex = 12;
+            PasswordTextBox1.TextOffset = new Size(0, 0);
+            PasswordTextBox1.UnderlinedStyle = true;
+            // 
+            // EmailTextBox1
+            // 
+            EmailTextBox1.BackColor = Color.Transparent;
+            EmailTextBox1.BackgroundColor = Color.White;
+            EmailTextBox1.BorderColor = Color.FromArgb(128, 128, 128, 128);
+            EmailTextBox1.Content = "";
+            EmailTextBox1.FocusBackgroundColor = Color.White;
+            EmailTextBox1.FocusBorderColor = Color.Black;
+            EmailTextBox1.FocusImageTint = Color.White;
+            EmailTextBox1.Font = new Font("Arial", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            EmailTextBox1.ForeColor = Color.Gray;
+            EmailTextBox1.Image = null;
+            EmailTextBox1.ImageExpand = new Point(0, 0);
+            EmailTextBox1.ImageOffset = new Point(0, 0);
+            EmailTextBox1.Location = new Point(46, 284);
+            EmailTextBox1.Margin = new Padding(4);
+            EmailTextBox1.Multiline = false;
+            EmailTextBox1.Name = "EmailTextBox1";
+            EmailTextBox1.NormalImageTint = Color.White;
+            EmailTextBox1.Padding = new Padding(15, 15, 15, 0);
+            EmailTextBox1.PasswordChar = false;
+            EmailTextBox1.PlaceholderColor = Color.Gray;
+            EmailTextBox1.PlaceholderText = "Email";
+            EmailTextBox1.Rounding = new Padding(8);
+            EmailTextBox1.Size = new Size(466, 45);
+            EmailTextBox1.TabIndex = 11;
+            EmailTextBox1.TextOffset = new Size(0, 0);
+            EmailTextBox1.UnderlinedStyle = true;
+            EmailTextBox1.ContentChanged += cuiTextBox2_ContentChanged;
+            // 
+            // NameTextBox2
+            // 
+            NameTextBox2.BackColor = Color.Transparent;
+            NameTextBox2.BackgroundColor = Color.White;
+            NameTextBox2.BorderColor = Color.FromArgb(128, 128, 128, 128);
+            NameTextBox2.Content = "";
+            NameTextBox2.FocusBackgroundColor = Color.White;
+            NameTextBox2.FocusBorderColor = Color.Black;
+            NameTextBox2.FocusImageTint = Color.White;
+            NameTextBox2.Font = new Font("Arial", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            NameTextBox2.ForeColor = Color.Gray;
+            NameTextBox2.Image = null;
+            NameTextBox2.ImageExpand = new Point(0, 0);
+            NameTextBox2.ImageOffset = new Point(0, 0);
+            NameTextBox2.Location = new Point(46, 219);
+            NameTextBox2.Margin = new Padding(4);
+            NameTextBox2.Multiline = false;
+            NameTextBox2.Name = "NameTextBox2";
+            NameTextBox2.NormalImageTint = Color.White;
+            NameTextBox2.Padding = new Padding(15, 15, 15, 0);
+            NameTextBox2.PasswordChar = false;
+            NameTextBox2.PlaceholderColor = Color.Gray;
+            NameTextBox2.PlaceholderText = "Last Name";
+            NameTextBox2.Rounding = new Padding(8);
+            NameTextBox2.Size = new Size(466, 45);
+            NameTextBox2.TabIndex = 10;
+            NameTextBox2.TextOffset = new Size(0, 0);
+            NameTextBox2.UnderlinedStyle = true;
+            // 
+            // NameTextBox1
+            // 
+            NameTextBox1.BackColor = Color.Transparent;
+            NameTextBox1.BackgroundColor = Color.White;
+            NameTextBox1.BorderColor = Color.FromArgb(128, 128, 128, 128);
+            NameTextBox1.Content = "";
+            NameTextBox1.FocusBackgroundColor = Color.White;
+            NameTextBox1.FocusBorderColor = Color.Black;
+            NameTextBox1.FocusImageTint = Color.White;
+            NameTextBox1.Font = new Font("Arial", 9.5F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            NameTextBox1.ForeColor = Color.Gray;
+            NameTextBox1.Image = null;
+            NameTextBox1.ImageExpand = new Point(0, 0);
+            NameTextBox1.ImageOffset = new Point(0, 0);
+            NameTextBox1.Location = new Point(46, 155);
+            NameTextBox1.Margin = new Padding(4);
+            NameTextBox1.Multiline = false;
+            NameTextBox1.Name = "NameTextBox1";
+            NameTextBox1.NormalImageTint = Color.White;
+            NameTextBox1.Padding = new Padding(15, 15, 15, 0);
+            NameTextBox1.PasswordChar = false;
+            NameTextBox1.PlaceholderColor = Color.Gray;
+            NameTextBox1.PlaceholderText = "First Name";
+            NameTextBox1.Rounding = new Padding(8);
+            NameTextBox1.Size = new Size(466, 45);
+            NameTextBox1.TabIndex = 9;
+            NameTextBox1.TextOffset = new Size(0, 0);
+            NameTextBox1.UnderlinedStyle = true;
+            // 
+            // RegisterButton
+            // 
+            RegisterButton.BackColor = Color.Transparent;
+            RegisterButton.CheckButton = false;
+            RegisterButton.Checked = false;
+            RegisterButton.CheckedBackground = Color.FromArgb(255, 106, 0);
+            RegisterButton.CheckedForeColor = Color.White;
+            RegisterButton.CheckedImageTint = Color.White;
+            RegisterButton.CheckedOutline = Color.FromArgb(255, 106, 0);
+            RegisterButton.Content = "Register";
+            RegisterButton.DialogResult = DialogResult.None;
+            RegisterButton.Font = new Font("Arial", 9.75F, FontStyle.Bold);
+            RegisterButton.ForeColor = Color.White;
+            RegisterButton.HoverBackground = Color.Transparent;
+            RegisterButton.HoveredImageTint = Color.Transparent;
+            RegisterButton.HoverForeColor = Color.Transparent;
+            RegisterButton.HoverOutline = Color.FromArgb(32, 128, 128, 128);
+            RegisterButton.Image = null;
+            RegisterButton.ImageAutoCenter = true;
+            RegisterButton.ImageExpand = new Point(0, 0);
+            RegisterButton.ImageOffset = new Point(0, 0);
+            RegisterButton.Location = new Point(46, 552);
+            RegisterButton.Name = "RegisterButton";
+            RegisterButton.NormalBackground = Color.Black;
+            RegisterButton.NormalForeColor = Color.White;
+            RegisterButton.NormalImageTint = Color.White;
+            RegisterButton.NormalOutline = Color.FromArgb(64, 128, 128, 128);
+            RegisterButton.OutlineThickness = 1F;
+            RegisterButton.Padding = new Padding(15);
+            RegisterButton.PressedBackground = Color.WhiteSmoke;
+            RegisterButton.PressedForeColor = Color.FromArgb(32, 32, 32);
+            RegisterButton.PressedImageTint = Color.White;
+            RegisterButton.PressedOutline = Color.Black;
+            RegisterButton.RightToLeft = RightToLeft.No;
+            RegisterButton.Rounding = new Padding(8);
+            RegisterButton.Size = new Size(466, 45);
+            RegisterButton.TabIndex = 8;
+            RegisterButton.TextAlignment = StringAlignment.Center;
+            RegisterButton.TextOffset = new Point(0, 0);
+            RegisterButton.Click += RegisterButton_Click;
+            // 
+            // SignupPanelText
+            // 
+            SignupPanelText.BackColor = Color.Transparent;
+            SignupPanelText.Content = "Register";
+            SignupPanelText.Font = new Font("Arial", 20F, FontStyle.Bold);
+            SignupPanelText.HorizontalAlignment = StringAlignment.Center;
+            SignupPanelText.Location = new Point(130, 70);
+            SignupPanelText.Margin = new Padding(4, 3, 4, 3);
+            SignupPanelText.Name = "SignupPanelText";
+            SignupPanelText.Size = new Size(300, 50);
+            SignupPanelText.TabIndex = 3;
+            SignupPanelText.VerticalAlignment = StringAlignment.Near;
+            // 
+            // SingupHeader
+            // 
+            SingupHeader.BackColor = Color.Transparent;
+            SingupHeader.Content = "Inventory\\ Management\\ System";
+            SingupHeader.Font = new Font("Arial", 36F, FontStyle.Bold);
+            SingupHeader.HorizontalAlignment = StringAlignment.Near;
+            SingupHeader.Location = new Point(88, 41);
+            SingupHeader.Margin = new Padding(4, 3, 4, 3);
+            SingupHeader.Name = "SingupHeader";
+            SingupHeader.Size = new Size(400, 200);
+            SingupHeader.TabIndex = 1;
+            SingupHeader.VerticalAlignment = StringAlignment.Near;
+            // 
+            // cuiLabel2
+            // 
+            cuiLabel2.BackColor = Color.Transparent;
+            cuiLabel2.Content = "Manage\\ your\\ products\\ easily\\ and\\ efficiently!";
+            cuiLabel2.Font = new Font("Arial", 10F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            cuiLabel2.HorizontalAlignment = StringAlignment.Near;
+            cuiLabel2.Location = new Point(88, 230);
+            cuiLabel2.Margin = new Padding(4, 3, 4, 3);
+            cuiLabel2.Name = "cuiLabel2";
+            cuiLabel2.Size = new Size(300, 54);
+            cuiLabel2.TabIndex = 2;
+            cuiLabel2.VerticalAlignment = StringAlignment.Near;
+            // 
+            // Signup
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightBlue;
+            ClientSize = new Size(1280, 720);
+            Controls.Add(cuiLabel2);
+            Controls.Add(SingupHeader);
+            Controls.Add(cuiPanel1);
+            Name = "Signup";
+            Text = "Inventory Management System: Signup";
+            Load += Signup_Load;
+            cuiPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private CuoreUI.Controls.cuiPanel cuiPanel1;
+        private CuoreUI.Controls.cuiLabel SingupHeader;
+        private CuoreUI.Controls.cuiLabel cuiLabel2;
+        private CuoreUI.Controls.cuiLabel SignupPanelText;
+        private CuoreUI.Controls.cuiButton RegisterButton;
+        private CuoreUI.Controls.cuiTextBox PasswordTextBox1;
+        private CuoreUI.Controls.cuiTextBox EmailTextBox1;
+        private CuoreUI.Controls.cuiTextBox NameTextBox2;
+        private CuoreUI.Controls.cuiTextBox NameTextBox1;
+        private CuoreUI.Controls.cuiTextBox PasswordTextBox2;
+        private CuoreUI.Controls.cuiCheckbox TaCCheckBox;
+    }
+}
---
+++ Login_Version1.0/Login_Version1.0/Signup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Login_Version1._0
+{
+    public partial class Signup : Form
+    {
+        public Signup()
+        {
+            InitializeComponent();
+        }
+
+        private void Signup_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cuiTextBox2_ContentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
